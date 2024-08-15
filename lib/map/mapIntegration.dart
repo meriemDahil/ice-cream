3b869b974f30335339031438ec004fd61eb704b9
meriem-da <meriemdahil31@gmail.com>
2024-08-15 23:46:13 +0200

    map using maptiler

--- lib/map/mapIntegration.dart
+++ lib/map/mapIntegration.dart
@@ -1,31 +1,43 @@
+
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapIntegration extends StatefulWidget {
-  const MapIntegration({super.key});
-
   @override
   State<MapIntegration> createState() => _MapIntegrationState();
 }
 
 class _MapIntegrationState extends State<MapIntegration> {
+  final String apiKey = '';
+  Dio dio =Dio();
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    return  FlutterMap(
-      options: MapOptions(initialCenter: LatLng( 1.2878, 103.8666),
-      initialZoom:11,
-      interactionOptions: InteractionOptions(flags: InteractiveFlag.doubleTapZoom),
+    return Scaffold(
+      appBar: AppBar(title: Text('MapTiler with Flutter MAP')),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(36.7965083, 2.9337917), 
+          initialZoom: 13.0,
+        ),
+        children: [
+         
+          TileLayer(
+            urlTemplate:
+                'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=$apiKey',
+            additionalOptions: {
+              'key': apiKey,
+            },
+          ),
+        ], 
       ),
-      children: [
-        openStreetMapTileLayer
-
-      ],);
+    );
   }
 }
-
-TileLayer get openStreetMapTileLayer => TileLayer(
-  urlTemplate:'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-  userAgentPackageName:'dev.fleaflet.flutter_map.example'
-  
-);
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_auth
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
+import location
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -20,5 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
